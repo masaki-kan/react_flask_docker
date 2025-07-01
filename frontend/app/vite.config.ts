21431307f594610c615bb7de88a3fb78b420f216
masaki-kan <changsu02han@gmail.com>
2025-07-02 00:31:13 +0900

    vit.config調整

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
-    https: false,
     proxy: {
       "/api": {
         target: "http://localhost:5001", // FlaskのAPIサーバー
