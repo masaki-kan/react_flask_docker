579a8f313e4459a6a19ef42d3e0de901a39e4160
masaki-kan <changsu02han@gmail.com>
2025-07-08 20:40:18 +0900

    承認ページ作成　機能も

--- frontend/app/src/api/tradeApi.ts
+++ frontend/app/src/api/tradeApi.ts
@@ -3,37 +3,6 @@ import { savedListType } from "../types/savedType";
 import { tradeApprovalListType } from "../types/approvalType";
 import { errorSweetalert2 } from "../component/common/alert/sweetalert2";
 
-export const tradeApi = async (
-  item_id: string,
-  buyer_id: string,
-  seller_id: string
-): Promise<{ result: boolean; message: string } | undefined> => {
-  try {
-    const response = await axios.post(
-      `${import.meta.env.VITE_API_URL}/api/trade`,
-      {
-        item_id: item_id,
-        buyer_id: buyer_id,
-        seller_id: seller_id,
-      }
-    );
-
-    return {
-      result: response.data.result,
-      message: response.data.message,
-    };
-  } catch (error: unknown) {
-    let errorMessage = "予期しないエラーが発生しました";
-
-    if (axios.isAxiosError(error) && error.response?.data?.error) {
-      errorMessage = error.response.data.error;
-    }
-
-    errorSweetalert2(errorMessage);
-    return;
-  }
-};
-
 export const getSavedList = async (
   user_id: string
 ): Promise<{ trades: savedListType[]; result: string } | undefined> => {
